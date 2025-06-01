65611698843ef6c4e62eb7e6e6f9d76b02fc150c
CoderTahmid <tahmid.ibne@gmail.com>
2025-06-01 14:44:26 +0600

    navbar done

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FaRegHeart } from "react-icons/fa";
+
+
+const Navbar = () => {
+    return (
+        <div className='bg-[#9538E2] m-[30px] rounded-4xl p-6 justify-between flex items-center'>
+            <NavLink to="/"><p className='text-white font-bold'>Gadget Heaven</p></NavLink>
+            <div className='flex text-white gap-[48px]'>
+                <NavLink to="/"><p>Home</p></NavLink>
+                <NavLink to="/statistics"><p>Statistics</p></NavLink>
+                <NavLink to="/dashboard"><p>Dashboard</p></NavLink>
+            </div>
+            <div className='flex gap-4'>
+                <div className='bg-white rounded-full flex justify-center items-center p-2'><AiOutlineShoppingCart /></div>
+                <div className='bg-white rounded-full flex justify-center items-center p-2'><FaRegHeart/></div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
     return (
-        <div>
-           kk 
-        </div>
+        <>
+            <Navbar></Navbar>
+            {/* Dynamic Section */}
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </>
     );
 };
 
