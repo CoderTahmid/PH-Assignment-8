5ff1b27f985f8e620506b37806b3744b1ff9ce51
CoderTahmid <tahmid.ibne@gmail.com>
2025-06-01 16:23:14 +0600

    Banner done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,9 +2,6 @@ import bannerImg from "../../assets/banner.jpg"
 
 const Banner = () => {
     return (
-        // <div className='h-[200px] bg-[#9538E2] mx-[30px]'>
-        //     Banner
-        // </div>
         <div>
             <div className="bg-[#9538E2] z-0 py-[48px] mx-[30px] space-y-[24px]">
                 <h1 className="text-white font-bold text-[56px] text-center">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
@@ -22,12 +19,6 @@ const Banner = () => {
                     <div
                         className="rounded-3xl h-[611px] text-white py-[68px] bg-cover bg-white flex justify-center items-center flex-col"
                         style={{ backgroundImage: `url(${bannerImg})` }}>
-                        {/* <h1 className="font-bold text-black text-[32px] mb-4">Subscribe to our Newsletter</h1>
-                        <p className="font-medium text-[20px] mb-6" style={{ color: "rgba(19, 19, 19, 0.7)" }}>Get the latest updates and news right in your inbox!</p>
-                        <div className="grid grid-cols-3 w-[60%]">
-                            <input type="text" className="border col-span-2 border-gray-200 rounded-xl text-gray-400 py-[18px] pl-[30px] mr-4" placeholder="Enter your email" />
-                            <button className="border border-red-100 text-black py-[18px] px-[30px] rounded-xl bg-[#E7FE29] hover:cursor-pointer">Subscribe</button>
-                        </div> */}
                     </div>
                 </div>
             </div>
